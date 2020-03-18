58b7ddcb0b5f8c5fa1a782395a9f495a44b57fb8
LG BeTeng <brian7.teng@gmail.com>
2020-03-18 00:46:29 -0500

    results propogate

--- yelp-page/src/App.js
+++ yelp-page/src/App.js
@@ -1,65 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './Form';
-import FetchAPI from './FetchAPI';
 import Header from './components/Header';
 
 const apiKey = process.env.REACT_APP_API_KEY;
-const apiEndpoint = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=chinese&location=60540&limit=5';
+
 class App extends Component {
   state = {
     loading: true,
     yelpData: {},
     yelpResults: [],
-    ready: false,
     search: '',
     apiEndpoint: ''
   }
 
   formInput = (search) => {
     this.setState({ search });
-    // this.setState({ apiEndpoint: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${search}&location=60540&limit=5` });
-    // console.log('from appjs', search);
-    // console.log('state', this.state.search);
-   this.getInfo(search);
+    this.getInfo(search);
   }
 
-  getInfo = async(search) => {
+  getInfo = async (search) => {
     const res = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${search}&location=60540&limit=5`, {
-              method: "GET",
-              headers: {
-                  "Authorization": `Bearer ${apiKey}`
-              }
-          })
-          const data = await res.json();
-          this.setState({ yelpData: data.businesses, loading: false })
-          console.log(this.state.yelpData);
-          console.log('');
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${apiKey}`
       }
-  
-
-
-  //   async componentDidMount() {
-  //     const res = await fetch(apiEndpoint, {
-  //         method: "GET",
-  //         headers: {
-  //             "Authorization": `Bearer ${apiKey}`
-  //         }
-  //     })
-  //     const data = await res.json();
-  //     this.setState({ yelpData: data.businesses, loading: false })
-  //     console.log(this.state.yelpData)
-  // }
+    })
+    const data = await res.json();
+    this.setState({ yelpData: data.businesses, loading: false })
+    console.log(this.state.yelpData);
+    console.log('');
+  }
 
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div>
+          <Header />
+          <Form formInput={this.formInput} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <Header />
         <Form formInput={this.formInput} />
+
+        <div>
+          {this.state.yelpData.map(entry => (
+            <div key={entry.id}>
+              <div>
+                {entry.name}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     )
+    
   }
-}
+
+
+} // component bracket
+
+
 
 export default App;
