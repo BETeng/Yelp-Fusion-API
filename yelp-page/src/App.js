89066dcd815ea24da6bd39b7610d17d737df4817
LG BeTeng <brian7.teng@gmail.com>
2020-03-17 22:08:26 -0500

    sucessful api call with searhc bar

--- yelp-page/src/App.js
+++ yelp-page/src/App.js
@@ -4,24 +4,60 @@ import Form from './Form';
 import FetchAPI from './FetchAPI';
 import Header from './components/Header';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+const apiEndpoint = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=chinese&location=60540&limit=5';
 class App extends Component {
   state = {
     loading: true,
     yelpData: {},
     yelpResults: [],
-    ready: false
+    ready: false,
+    search: '',
+    apiEndpoint: ''
   }
 
-  formInput = (search) =>{
-    console.log('from appjs', search)
+  formInput = (search) => {
+    this.setState({ search });
+    // this.setState({ apiEndpoint: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${search}&location=60540&limit=5` });
+    // console.log('from appjs', search);
+    // console.log('state', this.state.search);
+   this.getInfo(search);
   }
+
+  getInfo = async(search) => {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${search}&location=60540&limit=5`, {
+              method: "GET",
+              headers: {
+                  "Authorization": `Bearer ${apiKey}`
+              }
+          })
+          const data = await res.json();
+          this.setState({ yelpData: data.businesses, loading: false })
+          console.log(this.state.yelpData);
+          console.log('');
+      }
+  
+
+
+  //   async componentDidMount() {
+  //     const res = await fetch(apiEndpoint, {
+  //         method: "GET",
+  //         headers: {
+  //             "Authorization": `Bearer ${apiKey}`
+  //         }
+  //     })
+  //     const data = await res.json();
+  //     this.setState({ yelpData: data.businesses, loading: false })
+  //     console.log(this.state.yelpData)
+  // }
+
 
   render() {
-    return(
-    <div className="App">
-      <Header />
-      <Form formInput={this.formInput}/>
-    </div>
+    return (
+      <div className="App">
+        <Header />
+        <Form formInput={this.formInput} />
+      </div>
     )
   }
 }
--- yelp-page/src/Form.js
+++ yelp-page/src/Form.js
@@ -3,7 +3,8 @@ import FetchAPI from './FetchAPI';
 
 class Form extends Component {
     state = {
-        search: ''
+        search: '',
+        apiEndpoint:''
     }
 
 
@@ -14,8 +15,11 @@ class Form extends Component {
     handleSubmit = event => {
         event.preventDefault();
         this.props.formInput(this.state.search);
+        // this.props.formInput({ apiEndpoint: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${this.state.search}&location=60540&limit=5`})
         this.setState({ search: '' });
     }
+
+    
 
     render() {
         return (
